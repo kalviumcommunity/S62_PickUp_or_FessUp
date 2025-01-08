8b72fa173949877c28d1fe98349df2896899165a
Issac038 <issac.antony@kalvium.community>
2025-01-08 11:47:33 +0530

    done

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const mongoUrl = process.env.MONGO_URL
+
+mongoose.connect(mongoUrl)
+    .then(()=>{
+        console.log('Connected to database')
+    })
+    .catch(()=>{
+        console.log('Error connecting to database')
+    })
+
+app.use(express.json())
+
 app.get("/ping",(req,res)=>{
     return res.send("This is the ping route")
 })

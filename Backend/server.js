e7411c7c817c498ef27553b2868860c223861c88
Issac038 <issac.antony@kalvium.community>
2025-01-10 15:41:07 +0530

    connected to mongoClient

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
+const mongoose = require('mongoose')
 
+const PORT = process.env.PORT
 const mongoUrl = process.env.MONGO_URL
 
 mongoose.connect(mongoUrl)
@@ -18,6 +20,7 @@ app.get("/ping",(req,res)=>{
     return res.send("This is the ping route")
 })
 
-app.listen(3000,()=>{
-    console.log("Server is running on 3000")
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
+
 });
